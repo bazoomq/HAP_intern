d6e6cd6af295b109b7ec19ae0ba62deb3403b2f6
Loreta <36799053+Loreta2003@users.noreply.github.com>
2022-06-14 14:14:08 +0400

    Update ns.py

--- ns.py
+++ ns.py
@@ -51,18 +51,17 @@ def b(h):
     ro_atm, ro_gas = densities(h)[0], densities(h)[1]
     return g*(ro_atm - ro_gas)
 
-def get_sr():
-  r_list = np.linspace(0, rp_max, int(rp_max/0.001))
-  s_list = []
 
-  for r in r_list:
-    # equation for calculating S0
-    s = r * hyp2f1(1 / 4, 1 / 2, 5 / 4, (r / rp_max) ** 4)
-    s_list.append(s)
-  return r_list, s_list
+r_list = np.linspace(0, rp_max, int(rp_max/0.001))
+s_list = []
+
+for r in r_list:
+  # equation for calculating S0
+  s = r * hyp2f1(1 / 4, 1 / 2, 5 / 4, (r / rp_max) ** 4)
+  s_list.append(s)
 
 def f_interp1d(t):
-    rs, s_half = get_sr()
+    rs, s_half = r_list, s_list
     f = interp1d(s_half, rs, kind='cubic')
     return f(t)
 
@@ -104,8 +103,8 @@ def F(params):
     intg_rd += ds*r_ds[i]
   return (intg_theta/lam)**2 + (1+(2*np.pi*b(h)*intg_rztheta+2*np.pi*w*intg_rd)/(b_d*(lam**3)))**2
 
-h = 20300
-initial_guess = [0.59, 3.6]
+h = 15000
+initial_guess = [0.08, 7.5]
 result = optimize.fmin_cg(F, initial_guess)
 fitted_params = result
 print(degrees(fitted_params[0]))
