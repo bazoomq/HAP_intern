3b538cb9de6a2c13f2f9dbf9e48528a6cc9bb713
Loreta <36799053+Loreta2003@users.noreply.github.com>
2022-07-21 23:21:27 +0400

    Update natural_shape_balloon.py

--- natural_shape_balloon.py
+++ natural_shape_balloon.py
@@ -3,19 +3,22 @@ from math import degrees
 from optimization import *
 
 
-
 class Natural_Shape_Balloon:
   def __init__(self, h=15000, initial_theta= 0.08, initial_a = 7.5):
         self.h = h
         self.initial_guess = [initial_theta, initial_a]
-        
+        self.result = fmin_cg(F, self.initial_guess, args = [self.h], full_output=True)
 
-  def solve_z_r(self):
-    result = fmin_cg(F, self.initial_guess, args = [self.h])  
-    fitted_params = result
-    print(degrees(fitted_params[0]))
-    print(fitted_params[1])
-    z_sol, r_sol = Solve(fitted_params[0], fitted_params[1], self.h)[2], Solve(fitted_params[0], fitted_params[1], self.h)[3]
-    return z_sol, r_sol
-
-
+  def solve_z_r(self):   
+    params = self.result[0]
+    self.z_sol, self.r_sol = Solve(params[0], params[1], self.h)[2], Solve(params[0], params[1], self.h)[3]
+    return self.z_sol, self.r_sol
+  
+  def Volume(self):
+    v = 0
+    for i in range(len(self.z_sol)-1):
+      r1 = self.r_sol[i]
+      r2 = self.r_sol[i+1]
+      dz = self.z_sol[i+1] - self.z_sol[i]
+      v = v + np.pi*dz*(r1*r1 + r2*r2 + r1*r2)/3
+    return v
