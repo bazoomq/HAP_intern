7d61df71ed2dc9a8877d52732ed1091bb52d3515
spacev42 <vachik@bazoomq.org>
2023-05-04 06:22:03 +0000

    rewrite code without paralleling

--- grid_search.py
+++ grid_search.py
@@ -5,28 +5,44 @@ from solve import Solve
 from params import *
 
 
-def get_grid(theta_max, theta_min, p0_max, p0_min, step_theta, step_p0, rmax_in, velocity):
+def get_grid(params, rmax_in, velocity):
     """
     getting a grid to search theta0 and p0
     :param theta_max, theta_min, p0_max, p0_min: searching boundaries
     :param step_theta, step_p0: grid step  
     :return: grid of theta and p0 
     """
-    grid = []
+    
+    theta0_max, theta0_min, p0_max, p0_min, theta0_step, p0_step = params
+    optimal_z, optimal_r, loss_min = [], [], 1000
     rmax_out = 0
     rmax_tol = 1e-3
-    for i in np.arange(np.radians(theta_max), np.radians(theta_min), -np.radians(step_theta)):
-        for j in np.arange(p0_max, p0_min, -step_p0):
-            rmax_in = 3
-            while abs(rmax_out - rmax_in) > rmax_tol:
-                # if rmax_out != 0:
-                #     rmax_in = rmax_out
-                theta, _, z, r, p_gas, _ = Solve([i, j], rmax_in, velocity)
-                rmax_out = max(r)
-            
-            grid.append([[i, j], rmax_out])
-    return grid
+    #for i in np.arange(np.radians(theta0_max), np.radians(theta0_min), -np.radians(theta0_step)):
+    for j in np.arange(p0_max, p0_min, -p0_step):
+        rmax_in = 3
+        while abs(rmax_out - rmax_in) > rmax_tol:  
+            if rmax_out != 0:
+                rmax_in = rmax_out
+            theta, _, z, r, p_gas, _ = Solve([np.radians(1.22314215), j], rmax_in, velocity)
+            rmax_out = max(r)
+            r_diff = rmax_out - rmax_in
+            print(r_diff)
+        print("*********************************")
+        #grid.append([[i, j], rmax_out])
+        loss = np.sqrt((90 + np.degrees(theta[-1])) ** 2 + (r[-1]) ** 2) # rp_max - maximum possible radius of the balloon
+        if loss < loss_min:           
+            loss_min = loss
+            theta0, p0 = np.radians(1.22314215), j
+            theta_last = theta[-1]
+            r_last = r[-1]
+            optimal_z = z
+            optimal_r = r
+            optimal_pgas = p_gas
+            optimal_theta = theta
 
+    res = np.array([np.degrees(theta0), p0, theta_last, r_last, max(optimal_r), loss_min, optimal_z, optimal_r, optimal_theta, optimal_pgas])
+
+    return res            
 
 def theta0_p0(grid_params, rmax_in, velocity, number_of_cores):
     """
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from solve import Solve
 from density import density
 from params import *
-from grid_search import theta0_p0
+from grid_search import theta0_p0, get_grid
 import pandas as pd
 
 
@@ -15,10 +15,10 @@ def initialize(height):
     :return: min and max values and step of grid  
     """
     if height < 21500:
-        theta0_max = 1.2
-        theta0_min = 1.18
+        theta0_max = 1.25
+        theta0_min = 1.15
         p0_max = -14
-        p0_min = -14.3
+        p0_min = -14.5
         number_of_steps_p0 = 100
         number_of_steps_theta0 = 100
     else:
@@ -42,8 +42,7 @@ def main(number_of_cores, height):
     rho_atm, _, _, T_gas = density(height)
 
     p0_min, p0_max, number_of_steps_p0, theta0_min, theta0_max, number_of_steps_theta0 = initialize(height)
-    rmax_tol = 1e-3
-    mgas_tol = 1e-2
+    mgas_tol = 1e-1
     
     velocity = 2.9177
     
@@ -58,12 +57,13 @@ def main(number_of_cores, height):
 
         p0_min, p0_max, number_of_steps_p0, theta0_min, theta0_max, number_of_steps_theta0 = initialize(height)
 
-        number_of_recurse = 2
-        for i in range(number_of_recurse):
+        theta_last = 0 
+        r_last = 1
+        while (abs(np.degrees(theta_last) + 90) > 1e-2) or (abs(r_last) > 1e-2):
             theta0_step = (theta0_max - theta0_min) / number_of_steps_theta0
             p0_step = (p0_max - p0_min) / number_of_steps_p0
             
-            theta0, p0, theta_last, r_last, rmax_out, loss, z, r, theta, p_gas = theta0_p0([theta0_max, theta0_min, p0_max, p0_min, theta0_step, p0_step], rmax_in, velocity, number_of_cores)
+            theta0, p0, theta_last, r_last, rmax_out, loss, z, r, theta, p_gas = get_grid([theta0_max, theta0_min, p0_max, p0_min, theta0_step, p0_step], rmax_in, velocity)
             theta0_max, theta0_min = theta0 + theta0_step + epsilon, theta0 - theta0_step - epsilon
             p0_max, p0_min = p0 + p0_step + epsilon, p0 - p0_step - epsilon 
         
